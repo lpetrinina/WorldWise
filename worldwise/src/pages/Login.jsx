231371f2e7281628fa8fa077eca0b40073fd8cd3
Luiza Petrinina <petrinina95@gmail.com>
2025-08-21 17:34:06 +0300

    Add Page navigation

--- worldwise/src/pages/Login.jsx
+++ worldwise/src/pages/Login.jsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
+
 import styles from "./Login.module.css";
+import PageNav from "../components/PageNav";
 
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
@@ -7,22 +10,24 @@ export default function Login() {
 
   return (
     <main className={styles.login}>
+      <PageNav />
+
       <form className={styles.form}>
         <div className={styles.row}>
-          <label htmlFor="email">Email address</label>
+          <label htmlFor='email'>Email address</label>
           <input
-            type="email"
-            id="email"
+            type='email'
+            id='email'
             onChange={(e) => setEmail(e.target.value)}
             value={email}
           />
         </div>
 
         <div className={styles.row}>
-          <label htmlFor="password">Password</label>
+          <label htmlFor='password'>Password</label>
           <input
-            type="password"
-            id="password"
+            type='password'
+            id='password'
             onChange={(e) => setPassword(e.target.value)}
             value={password}
           />
