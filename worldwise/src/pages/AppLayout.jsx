e3b671575673e14ae4c3231ca44c1714eb663807
Luiza Petrinina <petrinina95@gmail.com>
2025-08-22 12:05:32 +0300

    Build the  app layout

--- worldwise/src/pages/AppLayout.jsx
+++ worldwise/src/pages/AppLayout.jsx
@@ -1,5 +1,14 @@
+import Map from "../components/layout/map/Map";
+import Sidebar from "../components/layout/sidebar/Sidebar";
+import styles from "./AppLayout.module.css";
+
 function AppLayout() {
-  return <div>Work</div>;
+  return (
+    <div className={styles.app}>
+      <Sidebar />
+      <Map />
+    </div>
+  );
 }
 
 export default AppLayout;
