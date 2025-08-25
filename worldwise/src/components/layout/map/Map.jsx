b421357d9d2e78d2d12eec3b593214efab9f02c0
Luiza Petrinina <petrinina95@gmail.com>
2025-08-25 14:33:22 +0300

    Add map component
    Use useNavigate hook to navigate to form

---
+++ worldwise/src/components/layout/map/Map.jsx
@@ -0,0 +1,24 @@
+import { useNavigate, useSearchParams } from "react-router";
+import styles from "./Map.module.css";
+
+function Map() {
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
+  return (
+    <div
+      className={styles.mapContainer}
+      onClick={() => navigate("form")}
+    >
+      <h1>Map</h1>
+      <h2>
+        Position: {lat}, {lng}
+      </h2>
+    </div>
+  );
+}
+
+export default Map;
