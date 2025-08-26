e7f64d0cb2e50cfe379a382c33776188b1e0156c
Luiza Petrinina <petrinina95@gmail.com>
2025-08-26 15:39:59 +0300

    Display city markers on map
    Add meaningful info for the city in popup

--- worldwise/src/components/layout/map/Map.jsx
+++ worldwise/src/components/layout/map/Map.jsx
@@ -8,9 +8,12 @@ import {
 
 import styles from "./Map.module.css";
 import { useState } from "react";
+import { useCities } from "../../../contexts/CitiesContext";
 
 function Map() {
   const navigate = useNavigate();
+  const { cities } = useCities();
+
   const [searchParams, setSearchParams] = useSearchParams();
   const [mapPosition, setMapPosition] = useState([40, 0]);
 
@@ -32,11 +35,21 @@ function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url='https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
         />
-        <Marker position={mapPosition}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+
+        {cities.map((city) => (
+          <Marker
+            position={[
+              city.position.lat,
+              city.position.lng,
+            ]}
+            key={city.id}
+          >
+            <Popup>
+              <span>{city.emoji}</span>
+              <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
       </MapContainer>
     </div>
   );
