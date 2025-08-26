25c093196518f6a1778fda2cc0016703b59d3551
Luiza Petrinina <petrinina95@gmail.com>
2025-08-26 12:56:43 +0300

    Change the style of the current city item

--- worldwise/src/components/city/CityItem.jsx
+++ worldwise/src/components/city/CityItem.jsx
@@ -1,4 +1,6 @@
+import { Link } from "react-router";
 import styles from "./CityItem.module.css";
+import { useCities } from "../../contexts/CitiesContext";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -8,16 +10,29 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function CityItem({ city }) {
-  const { cityName, emoji, date } = city;
+  const { currentCity } = useCities();
+  const { cityName, emoji, date, id, position } = city;
 
   return (
-    <li className={styles.cityItem}>
-      <span className={styles.emoji}>{emoji}</span>
-      <h3 className={styles.name}>{cityName}</h3>
-      <time className={styles.data}>
-        ({formatDate(date)})
-      </time>
-      <button className={styles.deleteBtn}>&times;</button>
+    <li>
+      <Link
+        className={`${styles.cityItem} ${
+          id === currentCity.id
+            ? styles["cityItem--active"]
+            : ""
+        }`}
+        to={`${id}?lat=${position.lat}&lng=${position.lng}`}
+      >
+        <span className={styles.emoji}>{emoji}</span>
+        <h3 className={styles.name}>{cityName}</h3>
+        <time className={styles.data}>
+          ({formatDate(date)})
+        </time>
+
+        <button className={styles.deleteBtn}>
+          &times;
+        </button>
+      </Link>
     </li>
   );
 }
