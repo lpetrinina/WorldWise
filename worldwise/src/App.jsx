fea19bd3df3d843cb75b27f997bcb7cc7c844dab
Luiza Petrinina <petrinina95@gmail.com>
2025-09-01 15:49:16 +0300

    Optimize the app loading
    Implement lazu loading

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -1,18 +1,22 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router";
 
-import Homepage from "./pages/Homepage";
-import Pricing from "./pages/Pricing";
-import Product from "./pages/Product";
-import Login from "./pages/Login";
-import AppLayout from "./pages/AppLayout";
+import { CitiesProvider } from "./contexts/CitiesContext";
+import { AuthProvider } from "./contexts/FakeAuthContext";
+
 import CityList from "./components/city/CityList";
 import CountryList from "./components/country/CountryList";
 import City from "./components/city/City";
 import Form from "./components/form/Form";
-import { CitiesProvider } from "./contexts/CitiesContext";
-import { AuthProvider } from "./contexts/FakeAuthContext";
 import ProtectedRoute from "./pages/ProtectedRoute";
-import PageNotFound from "./pages/PageNotFound";
+import SpinnerFullPage from "./components/common/spinner/SpinnerFullPage";
+
+const Homepage = lazy(() => import("./pages/Homepage"));
+const Pricing = lazy(() => import("./pages/Pricing"));
+const Product = lazy(() => import("./pages/Product"));
+const Login = lazy(() => import("./pages/Login"));
+const AppLayout = lazy(() => import("./pages/AppLayout"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound"));
 
 function App() {
   return (
@@ -20,16 +24,46 @@ function App() {
       <CitiesProvider>
         <BrowserRouter>
           <Routes>
-            <Route index element={<Homepage />} />
-            <Route path='pricing' element={<Pricing />} />
-            <Route path='product' element={<Product />} />
-            <Route path='login' element={<Login />} />
+            <Route
+              index
+              element={
+                <Suspense key={"home"} fallback={<SpinnerFullPage />}>
+                  <Homepage />
+                </Suspense>
+              }
+            />
+            <Route
+              path='pricing'
+              element={
+                <Suspense key={"pricing"} fallback={<SpinnerFullPage />}>
+                  <Pricing />
+                </Suspense>
+              }
+            />
+            <Route
+              path='product'
+              element={
+                <Suspense key={"product"} fallback={<SpinnerFullPage />}>
+                  <Product />
+                </Suspense>
+              }
+            />
+            <Route
+              path='login'
+              element={
+                <Suspense key={"login"} fallback={<SpinnerFullPage />}>
+                  <Login />
+                </Suspense>
+              }
+            />
 
             <Route
               path='app'
               element={
                 <ProtectedRoute>
-                  <AppLayout />
+                  <Suspense key={"app"} fallback={<SpinnerFullPage />}>
+                    <AppLayout />
+                  </Suspense>
                 </ProtectedRoute>
               }
             >
@@ -39,7 +73,14 @@ function App() {
               <Route path='countries' element={<CountryList />} />
               <Route path='form' element={<Form />} />
             </Route>
-            <Route path='*' element={<PageNotFound />} />
+            <Route
+              path='*'
+              element={
+                <Suspense key={"not-found"} fallback={<SpinnerFullPage />}>
+                  <PageNotFound />
+                </Suspense>
+              }
+            />
           </Routes>
         </BrowserRouter>
       </CitiesProvider>
